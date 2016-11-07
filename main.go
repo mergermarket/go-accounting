2e4898c16802b0aedec8e0a642f0c09732b64578
Chris James <qui666@gmail.com>
2016-11-07 13:29:41 +0000

    lets go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// Account represents a bank account
+type Account interface {
+
+	// Withdraw deducts amount from account returning the new balance
+	Withdraw(amount int) (newBalance int)
+
+	// Deposit adds amount to the account returning the new balance
+	Deposit(amount int) (newBalance int)
+}
